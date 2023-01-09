481486ccfd344a293f8fba02f70ca91ccc3425c3
Jose Storopoli <jose@storopoli.io>
2023-01-09 10:19:48 -0300

    fix formatting with .JuliaFormatter.toml

--- 2023-Geilo-Winter-School/01-Julia-Multiple-Dispatch.jl
+++ 2023-Geilo-Winter-School/01-Julia-Multiple-Dispatch.jl
@@ -8,8 +8,8 @@
 import Base: size, getindex
 
 struct OneHotVector <: AbstractVector{Int}
-        len::Int
-        ind::Int
+    len::Int
+    ind::Int
 end
 
 size(v::OneHotVector) = (v.len,)
@@ -19,24 +19,24 @@ getindex(v::OneHotVector, i::Integer) = Int(i == v.ind)
 # Since OneHotVector is a struct derived from AbstractVector we can use all of the methods
 # previously defined for AbstractVector and it simply works right off the bat.
 # Here we are constructing an Array with a list comprehension:
-onehot = [OneHotVector(3, rand(1:3)) for _ in 1:4]
+onehot = [OneHotVector(3, rand(1:3)) for _ = 1:4]
 
 # Define a new function inner_sum that is basically a recursive dot product with a summation:
 using LinearAlgebra
 
 function inner_sum(A, vs)
-        t = zero(eltype(A))
-        for v in vs
-                t += inner(v, A, v) # multiple dispatch!
-        end
-        return t
+    t = zero(eltype(A))
+    for v in vs
+        t += inner(v, A, v) # multiple dispatch!
+    end
+    return t
 end
 
 inner(v, A, w) = v ⋅ (A * w) # very general definition
 
 # It works!
 A = rand(3, 3)
-vs = [rand(3) for _ in 1:4]
+vs = [rand(3) for _ = 1:4]
 inner_sum(A, vs)
 
 # Why?
