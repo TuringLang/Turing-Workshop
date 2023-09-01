4812c13d75b984007ccb7e340ab367238ddfe1f1
Xianda Sun <sunxdt@gmail.com>
2023-09-01 18:56:28 +0100

    Add @torfjelde 's julia-setup doc

--- 2023-MRC/diffusion_driven_sir.jl
+++ 2023-MRC/diffusion_driven_sir.jl
@@ -23,17 +23,19 @@ function sir_ode!(du, u, p, t)
 end
 
 # Compute the PDF of the Ornstein-Uhlenbeck process at time t
-function ou_pdf(x, t, ξ1, ξ2, ξ3, x0)
+function ou_logdensity(x, t, ξ1, ξ2, ξ3, x0)
     μ = ξ1 / ξ2
     mean_t = μ + (x0 - μ) * exp(-ξ2 * t)
     variance_t = (ξ3^2) / (2 * ξ2) * (1 - exp(-2 * ξ2 * t))
-    return pdf(Normal(mean_t, sqrt(variance_t)), x)
+    return logpdf(Normal(mean_t, sqrt(variance_t)), x)
 end
 
 # Log density computation
 function LogDensityProblems.logdensity(prob::DiffusionDrivenSIR, parameters)
     @unpack N, y = prob
-    x₀, s₀, u₀ = parameters[1], parameters[2], parameters[3:end]
+    x₀, s₀, u₀, ws = parameters[1], parameters[2], parameters[3:6], parameters[7:end]
+
+    ws = reshape(ws, length(y), 10)
     S₀ = s₀ * N
     I₀ = N - S₀ # R₀ = 0 initially
     ξ1, ξ2, ξ3, γ = u₀
@@ -44,17 +46,16 @@ function LogDensityProblems.logdensity(prob::DiffusionDrivenSIR, parameters)
                  logpdf(Normal(ξ1 / ξ2, (ξ3^2) / (2 * ξ2)), x₀)
 
     # Initialize state variables
-    S, I, R, W, x = S₀, I₀, 0, 0.0, x₀
+    S, I, R, x = S₀, I₀, 0, x₀
 
     # Loop through time, simulating OU process and SIR ODE
     T = length(y)
     for t in 0.0:T-1
         # Simulate OU process
-        ou_process = NoiseProblem(OrnsteinUhlenbeckProcess(ξ1 - ξ2, ξ2, ξ3, t, W), (t, t + 1))
-        ou_sol = solve(ou_process, EM(), dt=0.1)
-        W = ou_sol.W[end]
-        x = ou_sol.u[end]
-        logdensity += ou_pdf(x, t, ξ1, ξ2, ξ3, x₀)
+        simulated_x = simulate_OU(ξ1, ξ2, ξ3, x, 9, ws[Int(t)+1, :])
+        x = simulated_x[end]
+        # logdensity += ou_logdensity(x, t, ξ1, ξ2, ξ3, x₀)
+        logdensity += cumsum(logpdf.(Normal(), ws[Int(t)+1, :]))
 
         # Simulate SIR ODE
         u = [S, I, R, x]
@@ -70,8 +71,27 @@ function LogDensityProblems.logdensity(prob::DiffusionDrivenSIR, parameters)
     return logdensity
 end
 
+
+
+function simulate_OU(ξ1, ξ2, ξ3, X0, l, ws_t)
+    dt = 1 / (l + 1)
+    N = l + 1
+    X = Vector{typeof(X0)}(undef, N+1)
+    X[1] = X0
+
+    for i in 2:N+1
+        dW = ws_t[i-1] * sqrt(dt)
+        drift = (ξ1 - ξ2 * X[i-1]) * dt
+        diffusion = ξ3 * dW
+        X[i] = X[i-1] + drift + diffusion
+    end
+
+    return X
+end
+
+
 function LogDensityProblems.dimension(prob::DiffusionDrivenSIR)
-    return 6
+    return 6 + length(prob.y) * 10
 end
 
 function LogDensityProblems.capabilities(prob::DiffusionDrivenSIR)
@@ -82,7 +102,60 @@ end
 y = [3, 8, 26, 76, 225, 298, 258, 233, 189, 128, 68, 29, 14, 4]
 
 prob = DiffusionDrivenSIR(763, y)
-params = rand(6)
+params = rand(LogDensityProblems.dimension(prob))
 
 # Test log density computation
 LogDensityProblems.logdensity(prob, params)
+
+using Turing
+using ReverseDiff
+using Zygote
+Turing.setadbackend(:forwarddiff)
+
+@model function DiffusionDrivenSIRModel(y, N, ::Type{T} = Float64) where T
+    # Hyperparameters for ξ1, ξ2, ξ3, γ
+    ξ1 ~ Gamma(2, 2)
+    ξ2 ~ Gamma(2, 2)
+    ξ3 ~ Gamma(2, 2)
+    γ ~ Gamma(2, 2)
+    
+    # Prior for s₀
+    s₀ ~ Beta(2, 1)
+
+    # Prior for x₀ based on the stationary distribution of the OU diffusion
+    x₀ ~ Normal(ξ1 / ξ2, (ξ3^2) / (2 * ξ2))
+
+    # Noise
+    ws ~ filldist(Normal(), length(y) * 10)
+
+    ws_matrix = reshape(ws, length(y), 10)
+    S₀ = s₀ * N
+    I₀ = N - S₀
+
+    # Initialize state variables
+    S, I, R, x = S₀, I₀, 0, x₀
+
+    # Loop through time, simulating OU process and SIR ODE
+    num_times = length(y)
+    for t in 0.0:num_times-1
+        # Simulate OU process
+        simulated_x = simulate_OU(ξ1, ξ2, ξ3, x, 9, ws_matrix[Int(t)+1, :])
+        x = simulated_x[end]
+
+        # Simulate SIR ODE
+        u = [S, I, R, x]
+        p = [ξ1, ξ2, ξ3, γ]
+        prob_sir = ODEProblem(sir_ode!, u, (t, t + 1), p)
+        sol_sir = solve(prob_sir; dt=1)
+        S, I, R = sol_sir.u[end][1:3]
+
+        # Observe the data
+        y[Int(t)+1] ~ Poisson(I)
+    end
+end
+
+model = DiffusionDrivenSIRModel(y, 763)
+
+rand(model)
+
+samples = sample(model, HMC(0.1, 25), 3000; nadapts = 1000)
---
+++ 2023-MRC/getting-started.md
@@ -0,0 +1,62 @@
+# Getting started
+
+These workshops are intended to be somewhat interactive, and so you might be at risk of having to actually write and execute some code on your own device!
+
+If you're already familiar with Julia and have Julia 1.8 or greater already installed, then you can probably stop reading here.
+
+If you're new to Julia, you will unfortunately have to install yet another language on your device. Luckily, it's a fairly straight forward mission.
+
+
+
+## "Woah there! I have to *execute* Julia code? HOW?!" - Installing Julia
+
+We'll be using Julia **1.8.x** throughout, so we recommend you use the same version to avoid any issues.
+
+**Julia can be downloaded at the official website (https://julialang.org/downloads/) and a more detailed instructions can also be found there (https://julialang.org/downloads/platform/).**
+
+At the end of this process you should have access to some way of getting you into the Julia REPL which should present something similar to this:
+
+```julia
+               _
+   _       _ _(_)_     |  Documentation: https://docs.julialang.org
+  (_)     | (_) (_)    |
+   _ _   _| |_  __ _   |  Type "?" for help, "]?" for Pkg help.
+  | | | | | | |/ _` |  |
+  | | |_| | | | (_| |  |  Version 1.8.2 (2022-09-29)
+ _/ |\__'_|_|_|\__'_|  |  Official https://julialang.org/ release
+|__/                   |
+
+julia> 
+```
+
+For example, on a Linux device this is generally achieved by running the command `julia` or the executable `/wherever/you/installed/julia/bin/julia`.
+
+(If the version is slightly different, e.g. 1.8.4, that is no problem; the important bit is that you're on a version ≥1.8)
+
+## "But how do I *write* Julia code?!" - Setting up an editor
+
+As mentioned earlier, at certain points during these workshops you're likely to find yourself in a position where you even need to *write* some Julia code. I know, this is all very demanding.
+
+How and where and with what you want to write these chunks of text is of course completely up to yourself.
+
+- If you live and die by Notepad++, then you're welcome to use Notepad++.
+- If you're one of those wizards who doesn't have to spend 30 minutes trying to exit a file you opened in `vim`, then maybe you should use `vim`.
+- Or if you're part of the opposition of the group of people described in the previous line, i.e. an `emacs` user, then you might want to use `emacs`.
+
+**But most (sane) people will probably prefer something like:**
+1. a proper IDE, in which case [Visual Studio (VS) Code](https://code.visualstudio.com/) with [the Julia extensions](https://www.julia-vscode.org/) is a great option, or
+2. [Jupyter notebooks](https://jupyter.org/), which is easily supported by simply installing the Julia package `IJulia`; once this is installed you should find a `julia` kernel available in your notebook.[^1]
+
+The latter option might be more familiar if you're coming from a scientific Python background, but VS Code is generally the recommended option.
+
+Amongst us speakers we're both familiar with Jupyter notebooks, José is a VS Code and neovim user, and Tor is a (grumpy) `emacs` user, hence we should be able to provide _some_ support with a few different setups.
+
+Note that we expect everyone to act civil during the workshops, regardless of the differences in choices of editor people make. 
+In particular, any criticism of `emacs` will be met by loud boos and shaking of fists from Tor in an attempt to quell potential conflicts.
+
+# Additional resources
+- Julia Language website: https://www.julialang.org
+- Turing website: https://turing.ml/stable/
+- Julia Language YouTube channel: https://www.youtube.com/thejulialanguage
+
+[^1]: Alternatively, one can start the notebook from the Julia REPL by running `using IJulia; IJulia.notebook()`.
