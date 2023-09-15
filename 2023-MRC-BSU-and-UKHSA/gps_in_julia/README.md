af6b8f3db69f8b6fd30ff50bff31999bf211bfb9
Will Tebbutt <wt0881@my.bristol.ac.uk>
2023-09-15 14:09:32 +0100

    Work

---
+++ 2023-MRC-BSU-and-UKHSA/gps_in_julia/README.md
@@ -0,0 +1,8 @@
+# GP Session
+
+Please activate this folder, instantiate the packages, and run
+```julia
+include("script.jl")
+putting_example()
+```
+in order to check that everything is working as intended.
--- 2023-MRC-BSU-and-UKHSA/gps_in_julia/script.jl
+++ 2023-MRC-BSU-and-UKHSA/gps_in_julia/script.jl
@@ -15,13 +15,12 @@ using AbstractGPs, CSV, DataDeps, DataFrames, KernelFunctions, LinearAlgebra,
 #
 # Overall plan:
 # 1. intro + (trivial) complete example (~10 mins)
-# 2. start from basics and build towards complete example (~45 mins)
+# 2. start from basics and build towards complete example (~35 mins)
 #    - brief intro to GPs
 #    - KernelFunctions.jl
 #    - AbstractGPs.jl
 #    - strategies for utilising GPs in Turing
-# 3. free-form time to work on whatever is interesting (~35 mins)
-#    - some curated / datasets will be provided
+# 3. free-form time to work on whatever is interesting, and clarify and issues (~15 mins)
 #    - you might also expand on anything you've found interesting thus far in the session
 
 # We're going to use Zygote to perform AD.
@@ -35,17 +34,14 @@ register(DataDep(
     "fc28d83896af7094d765789714524d5a389532279b64902866574079c1a977cc",
 ));
 
-@model function putting_model(d, d_pred, n, y)
+@model function putting_model(d, n)
     v ~ InverseGamma(2, 3)
     l ~ InverseGamma(2, 3)
     f = GP(v * with_lengthscale(SEKernel(), l))
     fx = f(d, 1e-4)
     f_latent ~ fx
     y ~ Product(Binomial.(n, logistic.(f_latent)))
-
-    # Generated quantities.
-    fx_pred = rand(posterior(fx, f_latent)(d_pred, 1e-4))
-    return Zygote.dropgrad((fx_pred=fx_pred, p=logistic.(fx_pred)))
+    return (fx=fx, f_latent=f_latent)
 end
 
 function putting_example()
@@ -56,13 +52,19 @@ function putting_example()
 
     # Construct a simple model with a latent GP.
     d_pred = 1:0.2:21
-    model = putting_model(df.distance, d_pred, df.n, df.y)
+    model = putting_model(df.distance, df.n) | (y=df.y, )
     chn = sample(Xoshiro(123456), model, NUTS(), 100)
 
     # Plot probs of success.
-    L = reduce(hcat, map(x -> x.p, generated_quantities(model, chn)))
+    samples = map(generated_quantities(model, chn)) do x
+        return logistic.(rand(posterior(x.fx, x.f_latent)(d_pred, 1e-4)))
+    end
     p = plot()
-    plot!(d_pred, L; label="", color=:blue, alpha=0.2)
+    plot!(d_pred, reduce(hcat, samples); label="", color=:blue, alpha=0.2)
     scatter!(df.distance, df.y ./ df.n; label="", color=:red)
     savefig(p, "putting_success_probs.png")
 end
+
+# Notes:
+# 6. How to use block gibbs?
+# 7. When do you not want to use these abstractions? e.g. fixed kernel matrix, variable length scale across all dims.
